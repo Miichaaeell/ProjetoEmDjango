0d313307817db263ef3d2ecd73f3bb1cd8d3d305
Michael Andrew <96355111+Miichaaeell@users.noreply.github.com>
2024-10-17 21:35:10 -0300

    host DB interno

--- app/settings.py
+++ app/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME': 'usuarios_q1cr',
         'USER':environ.get('DB_USER'),
         'PASSWORD':environ.get('DB_PASSWORD'),
-        'HOST':environ.get('DB_HOST'),
+        'HOST':environ.get('BD_HOST_INTERNO'),
         'PORT':environ.get('DB_PORT'),
     }
 }
