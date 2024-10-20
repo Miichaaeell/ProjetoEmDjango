8d322e48d2f765382653d9294931b0965973655f
Michael Andrew <96355111+Miichaaeell@users.noreply.github.com>
2024-10-20 20:53:48 -0300

    routing.py

--- main/static/js/base.js
+++ main/static/js/base.js
@@ -1,4 +1,4 @@
-const chatSocket = new WebSocket(`ws://${window.location.host}/ws/chat-socket/`)
+const chatSocket = new WebSocket(`wss://${window.location.host}/ws/chat-socket/`)
 Notification.requestPermission()
 const img = '../image/whats_ico.ico'
 
