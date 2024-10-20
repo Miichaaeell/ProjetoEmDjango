61d4405b3f488612eaa0c5e2b015f305471275c7
Michael Andrew <96355111+Miichaaeell@users.noreply.github.com>
2024-10-20 12:29:21 -0300

    Update asgi.py

--- app/asgi.py
+++ app/asgi.py
@@ -13,8 +13,7 @@ django_asgi_app = get_asgi_application()
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.urls import path
-from boot.consumers import NotificationConsumer
+import boot.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
@@ -22,11 +21,7 @@ application = ProtocolTypeRouter({
     'http':django_asgi_app,
     'websocket': AllowedHostsOriginValidator(
             AuthMiddlewareStack(
-                URLRouter(
-                    path("", NotificationConsumer.as_asgi()),
-                    path("chat", NotificationConsumer.as_asgi()),
-                    path("conversa/<int:id_cliente>", NotificationConsumer.as_asgi())
-                )  
+                URLRouter(boot.routing.websocket_urlpatterns.as_asgi())  
                 )
             )
         }
